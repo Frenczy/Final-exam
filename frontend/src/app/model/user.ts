07da59b1a124f0373498812302ef5a0ceec44daa
Kiss Ferenc <69081242+Frenczy@users.noreply.github.com>
2021-08-16 21:15:26 +0200

    Delete frontend directory

--- frontend/src/app/model/user.ts
+++
@@ -1,8 +0,0 @@
-export class User {
-    // MongoDB miatt string
-    _id:string = "";
-    firstname:string="";
-    lastname:string="";
-    email:string="";
-    address:string="";
-    active:boolean=true}
